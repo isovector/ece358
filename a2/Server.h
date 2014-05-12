1313d3041287ec6f85eb81a524a6fb23c265c1b2
Ariel Weingarten <asweingarten@gmail.com>
2014-05-12 18:20:15 -0400

    Added comments and fixed indentation

--- a2/Server.h
+++ a2/Server.h
@@ -12,19 +12,22 @@ class Server
 		Server() { }
 		virtual ~Server() { }
 
-    virtual void run() = 0;
+    	virtual void run() = 0;
 		void add_student(int group_id, int student_id, std::string name);
 
 	protected:
-    int get_port() const;
+		// Initialization
+    	int get_port() const;
 
-    virtual bool handle_msg(int client, const char *msg) = 0;
-    virtual void respond(int client, std::string reply) = 0;
+    	// Message handling methods
+    	virtual bool handle_msg(int client, const char *msg) = 0;
+    	virtual void respond(int client, std::string reply) = 0;
 
+    	// Client handling methods
 		void get(int group_id, int student_id);
 		virtual void stop_session() = 0;
 		virtual void stop() = 0;
 
-    std::map<int, std::map<int, std::string> > groups_;
+    	std::map<int, std::map<int, std::string> > groups_;
 };
 
