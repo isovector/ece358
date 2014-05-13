d6866273bb08d5d28b268285b05a636233f3a80e
Ariel Weingarten <asweingarten@gmail.com>
2014-05-13 14:54:38 -0400

    Changed permissions and added functions
    Added a protected send_stop method.
    
    Moved send_get and send_stop_session into protected rather than private.
    
    Added a set_host_info method along with variables to store the
    information passed in.

--- a2/Client.h
+++ a2/Client.h
@@ -10,9 +10,18 @@ class Client
 		Client();
 		~Client();
 
-	private:
-		void send_get( int group_id, int student_id );
+	protected:
+		void send_get(int group_id, int student_id);
 		void send_stop_session();
+		void send_stop();
 
 		virtual std::string send_message() = 0;
+
+
+		void set_host_info(std::string host_address, int host_port);
+
+		std::string host_address;
+		int host_port;
+
+	private:
 };
