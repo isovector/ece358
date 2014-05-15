12e738b26b8b4f5aea6efcd90faf85bdf24f29ce
Sandy Maguire <sandy@sandymaguire.me>
2014-05-15 12:07:41 -0400

    UDPClient binds to requested host

--- a2/UDPClient.cc
+++ a2/UDPClient.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <errno.h>
+#include <arpa/inet.h>
 using namespace std;
 
 UDPClient::UDPClient()
@@ -21,6 +22,7 @@ UDPClient::~UDPClient()
 
 string UDPClient::send_message(Message msg)
 {
+  // Send the message to sock_
   int result = sendto(
     sock_, 
     msg.serialize(), 
@@ -31,6 +33,7 @@ string UDPClient::send_message(Message msg)
   );
 
   if (result != sizeof(Message)) {
+    // We didn't send as many bytes as we expected. Die in a fire!
     cerr << "error: failed to send entire message" << endl;
     exit(1);
   }
@@ -38,6 +41,7 @@ string UDPClient::send_message(Message msg)
   static const size_t BUFFER_SIZE = 1024;
   char buffer[BUFFER_SIZE];
 
+  // Block until we have received a response
   socklen_t addrLength = sizeof(sockaddr_in);
   result = recvfrom(
     sock_, 
@@ -54,6 +58,7 @@ string UDPClient::send_message(Message msg)
 
 void UDPClient::set_host_info( string host_address, int host_port )
 {
+  // Create a UDP socket
   sock_ = socket(PF_INET, SOCK_DGRAM, 0);
 
   if (sock_ == -1) {
@@ -61,19 +66,23 @@ void UDPClient::set_host_info( string host_address, int host_port )
     exit(1);
   }
 
+  int result;
+
   // Determine our endpoint for the socket
-  // TODO(sandy): figure out how to do this properly
   memset(&addr_, 0, sizeof(sockaddr_in));
   addr_.sin_family = AF_INET;
   addr_.sin_port = htons(host_port);
-  addr_.sin_addr.s_addr = htonl(INADDR_ANY);
+  result = inet_pton(AF_INET, host_address.c_str(), &addr_.sin_addr.s_addr);
+  if (result == 0) {
+    // inet_pton returns 0 for failed hostname
+    cerr << "error: bad host_address given: " << host_address << endl;
+    exit(1);
+  }
 
   // Bind and listen to the socket
-  int result;
   result = bind(sock_, reinterpret_cast<sockaddr*>(&addr_), sizeof(sockaddr_in));
   if (result < 0) {
-    cerr << "error: unable to bind to address " << errno << endl;
-    close(sock_);
+    cerr << "error: unable to bind to address" << endl;
     exit(1);
   }
 }
