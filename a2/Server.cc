b050c97f37f6c91cbb3ebe1f71c83d2e3ee3071e
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-05-14 22:02:00 -0400

    Added comments and added Destructor
    Also moved opening new brackets to new line.

--- a2/Server.cc
+++ a2/Server.cc
@@ -17,19 +17,22 @@ Server::Server() {
 
   // Necessary to maintain head of linked list so we can delete it later
   ifap = ifap_head;
-  do {
+  do
+  {
     // Loop through them, getting their ip addresses and interface names
     sockaddr_in *sa = (sockaddr_in *) ifap->ifa_addr;
     ip = inet_ntoa(sa->sin_addr);
 
-    if (is_good_interface(ifap->ifa_name, ip)) {
+    if (is_good_interface(ifap->ifa_name, ip))
+    {
       // If this is an interface we want, let's use it!
       break;
     }
   } while ((ifap = ifap->ifa_next));
 
   freeifaddrs(ifap_head);
-  if (!ifap) {
+  if (!ifap)
+  {
     // Failed to get any sick interfaces. Die in a fire.
     cerr << "error: no good interfaces" << endl;
     exit(1);
@@ -38,13 +41,31 @@ Server::Server() {
   address_ = ip;
 }
 
+Server::~Server()
+{
+
+}
+
+/*
+	Builds host's connection string
+	Returns:
+		string - "<address>:<port>"
+*/
 string Server::get_client_connection_string() const {
   char buffer[16];
   sprintf(buffer, "%d", port_);
   return address_  + ":" + buffer;
 }
 
-bool Server::is_good_interface(std::string name, std::string ip) {
+/*
+	Used to filter unusable interfaces
+	Input:
+		string name - name of interface being checked
+		string ip   - IP address of interface being checked
+	Returns:
+		bool - is this a usable interface?
+*/
+bool Server::is_good_interface(string name, string ip) {
   if (name != "wlan0" && name != "eth0") {
     return false;
   }
@@ -52,12 +73,26 @@ bool Server::is_good_interface(std::string name, std::string ip) {
   return ip.substr(0, 3) == "10.";
 }
 
+/*
+	Adds a student to a group
+	Input:
+		int group_id   - group student should be added to
+		int student_id - student_id of student being added
+		string name    - name of the student to be added
+*/
 void Server::add_student(int group_id, int student_id, string name)
 {
   groups_[group_id][student_id] = name;
 }
 
-string Server::get(int group_id, int student_id) {
+/*
+	Constructs response to clients' get requests
+	Input:
+		int group_id   : group that requested student is in
+		int student_id : id of student whose name is desired by client
+*/
+string Server::get(int group_id, int student_id)
+{
   if (groups_.count(group_id) && groups_[group_id].count(student_id)) {
     return groups_[group_id][student_id];
   }
@@ -67,7 +102,13 @@ string Server::get(int group_id, int student_id) {
   return str.str();
 }
 
-int Server::get_port() const {
+/*
+	Finds an available port
+	Returns:
+		int - available port number
+*/
+int Server::get_port() const
+{
   // TODO: figure out how to do this properly
   return 1337;
 }
