b4e10accb18188ca3af677e688f16e3175a214f3
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-05-21 19:32:39 -0400

    Added comments
    Also reformatted existing function description comments

--- a2/Server.cc
+++ a2/Server.cc
@@ -8,6 +8,8 @@
 #include <net/if.h>
 using namespace std;
 
+//  Description:
+//    The constructor identifies a suitable interface to listen on
 Server::Server() {
   // Load up all of the network interfaces
   ifaddrs *ifap, *ifap_head;
@@ -44,25 +46,24 @@ Server::~Server()
 
 }
 
-/*
-	Builds host's connection string
-	Returns:
-		string - "<address> <port>"
-*/
+
+//  Description:
+//    Builds host's connection string
+//  Returns:
+// 	  string : "<address> <port>"
 string Server::get_client_connection_string() const {
   char buffer[16];
   sprintf(buffer, "%d", port_);
   return address_  + " " + buffer;
 }
 
-/*
-	Used to filter unusable interfaces
-	Input:
-		string name - name of interface being checked
-		string ip   - IP address of interface being checked
-	Returns:
-		bool - is this a usable interface?
-*/
+//  Description:
+//    Used to filter unusable interfaces
+//  Input:
+// 	  string name : name of interface being checked
+// 	  string ip   : IP address of interface being checked
+//  Returns:
+// 	  bool : is this a usable interface?
 bool Server::is_good_interface(ifaddrs *ifap) {
   if ((ifap->ifa_flags & IFF_LOOPBACK) != 0) {
     // This is a loopback address, not what we want
@@ -77,24 +78,24 @@ bool Server::is_good_interface(ifaddrs *ifap) {
   return true;
 }
 
-/*
-	Adds a student to a group
-	Input:
-		int group_id   - group student should be added to
-		int student_id - student_id of student being added
-		string name    - name of the student to be added
-*/
+
+//  Description:
+//    Adds a student to a group.
+//  Input:
+// 	  int group_id   : group student should be added to
+// 	  int student_id : student_id of student being added
+// 	  string name    : name of the student to be added
 void Server::add_student(int group_id, int student_id, string name)
 {
   groups_[group_id][student_id] = name;
 }
 
-/*
-	Constructs response to clients' get requests
-	Input:
-		int group_id   : group that requested student is in
-		int student_id : id of student whose name is desired by client
-*/
+
+//  Description:
+//    Constructs response to clients' get requests.
+//  Input:
+// 	  int group_id   : group that requested student is in
+// 	  int student_id : id of student whose name is desired by client
 string Server::get(int group_id, int student_id)
 {
   if (groups_.count(group_id) && groups_[group_id].count(student_id)) {
@@ -109,6 +110,12 @@ string Server::get(int group_id, int student_id)
 #define	PORT_RANGE_LO	10000
 #define PORT_RANGE_HI	10100
 
+//  Description:
+//    Our implementation of the "mybind.c" code released by the prof
+//  Input:
+//    int sockfd        : the server's socket file descriptor
+//    sockaddr_in *addr : address the server's socket will listen on
+//    size_t size       : size of the addr structure
 int Server::get_port_and_bind(int sockfd, struct sockaddr_in *addr, size_t size) {
   if(sockfd < 1) {
     fprintf(stderr, "get_port_and_bind(): sockfd has invalid value\n");
