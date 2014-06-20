4a55e26a1f44c62d3139b0161ee3aefb7fab8a27
Sandy Maguire <sandy@sandymaguire.me>
2014-06-20 17:37:35 -0400

    Added rcs skeleton

---
+++ a4/rcs.cc
@@ -0,0 +1,40 @@
+#include "rcs.h"
+
+extern int errno;
+
+int rcsSocket() {
+    return 0;
+}
+
+int rcsBind(int sockfd, const struct sockaddr_in *addr) {
+    return 0;
+}
+
+int rcsGetSockName(int sockfd, struct sockaddr_in *addr) {
+    return 0;
+}
+
+int rcsListen(int sockfd) {
+    return 0;
+}
+
+int rcsAccept(int sockfd, struct sockaddr_in *addr) {
+    return 0;
+}
+
+int rcsConnect(int sockfd, const struct sockaddr_in *addr) {
+    return 0;
+}
+
+int rcsRecv(int sockfd, void *data, int size) {
+    return 0;
+}
+
+int rcsSend(int sockfd, const void *data, int size) {
+    return 0;
+}
+
+int rcsClose(int sockfd) {
+    return 0;
+}
+
