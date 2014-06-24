940b9ca984c6dc632bd2b02fd1ddac4f4f577ccf
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-06-24 18:33:39 -0400

    Added missing function parameters.

--- a4/rcs.cc
+++ a4/rcs.cc
@@ -63,7 +63,7 @@ int rcsAccept(int sockfd, struct sockaddr_in *addr) {
         const int LENGTH_OF_SYN = 8;
         char buffer[LENGTH_OF_SYN];
 
-        int bytes_received = SOCKET.recv()
+        int bytes_received = SOCKET.recv(buffer, LENGTH_OF_SYN);
 
         buffer[bytes_received] = 0;
         std::string syn = "buffer";
