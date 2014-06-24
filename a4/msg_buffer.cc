6a85b0c537198767aef19690e85935e4bc1e1dc6
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-06-24 18:36:53 -0400

    Added function comments.

--- a4/msg_buffer.cc
+++ a4/msg_buffer.cc
@@ -11,15 +11,29 @@ msg_buffer_t::msg_buffer_t() :
 {
 }
 
+//  Description:
+//    Queues a message in the buffer and updates the count
+//    of total bytes in the buffer
+//  Input:
+//     const msg_t &msg : message to be queued
 void msg_buffer_t::queueMessage(const msg_t &msg) {
     buffer_.push_back(const_cast<msg_t&>(msg));
     totalSize_ += msg.length;
 }
 
+//  Description:
+//    Checks if the buffer is empty
+//  Output:
+//    bool : is the buffer empty?
 bool msg_buffer_t::empty() const {
     return totalSize_ == 0;
 }
 
+//  Description:
+//    Read bytes out of the buffer.
+//  Input:
+//    char *dest    : buffer to be filled with data
+//    size_t length : number of bytes to read from buffer.
 size_t msg_buffer_t::read(char *dest, size_t length) {
     assert(!empty());
     size_t written = 0;
@@ -38,9 +52,9 @@ size_t msg_buffer_t::read(char *dest, size_t length) {
         size_t toCopy = min(length, front.length - offset_);
 
         memcpy(
-            dest, 
+            dest,
             front.data + offset_,
-            toCopy 
+            toCopy
         );
 
         dest += toCopy;
