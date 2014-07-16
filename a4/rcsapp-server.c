d8cd6a9877b69926e329e155d7db432830f17dc1
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-16 17:41:10 -0400

    Latest version from LEARN. #fuckthisshit

--- a4/rcsapp-server.c
+++ a4/rcsapp-server.c
@@ -1,10 +1,10 @@
 /* Mahesh V. Tripunitara
- *  * University of Waterloo
- *   * tcp-server.c -- first prints out the IP & port on which in runs.
- *    * Then awaits connections from clients. Create a pthread for each
- *     * connection. The thread just reads what the other end sends and
- *      * write()'s it to a file whose name is the thread's ID.
- *       */
+ * University of Waterloo
+ * tcp-server.c -- first prints out the IP & port on which in runs.
+ * Then awaits connections from clients. Create a pthread for each
+ * connection. The thread just reads what the other end sends and
+ * write()'s it to a file whose name is the thread's ID.
+ */
 
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -26,13 +26,14 @@
 
 
 extern int rcsSocket();
-extern int rcsBind(int,struct sockaddr_in *);
+extern int rcsBind(int, struct sockaddr_in *);
+extern int rcsGetSockName(int, struct sockaddr_in *);
 extern int rcsListen(int);
-extern int rcsAccept (int , struct sockaddr_in *);
-extern int rcsConnect (int , const struct sockaddr_in *);
-extern int rcsRecv (int , void *, int );
-extern int rcsSend(int,const void *,int);
-extern int mybind(int, struct sockaddr_in *);
+extern int rcsAccept (int, struct sockaddr_in *);
+extern int rcsConnect (int, const struct sockaddr_in *);
+extern int rcsRecv (int, void *, int);
+extern int rcsSend(int, const void *, int);
+extern int rcsClose(int);
 
 void *serviceConnection(void *arg) {
     int s = *(int *)arg;
@@ -40,65 +41,69 @@ void *serviceConnection(void *arg) {
     char fname[256];
     memset(fname, 0, 256);
     snprintf(fname, 255, "%lu", pthread_self());
-
+    
     int wfd = open(fname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-
+    
     if(wfd < 0) {
         perror("open"); return NULL;
     }
-
+    
     unsigned char buf[256];
     ssize_t recvlen = 0;
     while((recvlen = rcsRecv(s, buf, 256)) >= 0) {
 #ifdef _DEBUG_
         if(recvlen > 0) {
             printf("%lu received %d bytes.\n",
-                    pthread_self(), recvlen);
+                   pthread_self(), recvlen);
         }
 #endif
-
+        
         if(recvlen <= 0) {
             /* quit */
 #ifdef _DEBUG_
             printf("%lu exiting, spot 1...\n", pthread_self());
 #endif
             close(wfd);
+	    rcsClose(s);
             return NULL;
         }
-
+        
 #if 0
         if(write(STDOUT_FILENO, buf, recvlen) < recvlen) {
 #endif
             if(write(wfd, buf, recvlen) < recvlen) {
                 perror("write() in thread wrote too few");
                 close(wfd);
+		rcsClose(s);
                 return NULL;
             }
         }
-
+        
 #ifdef _DEBUG_
         printf("%lu exiting, spot 2...\n", pthread_self());
 #endif
         close(wfd);
+	rcsClose(s);
         return NULL;
     }
-
+    
     uint32_t getPublicIPAddr() {
         struct ifaddrs *ifa;
-
+        
         if(getifaddrs(&ifa) < 0) {
             perror("getifaddrs"); exit(0);
         }
-
+        
         struct ifaddrs *c;
         for(c = ifa; c != NULL; c = c->ifa_next) {
             if(c->ifa_addr == NULL) continue;
             if(c->ifa_addr->sa_family == AF_INET) {
                 struct sockaddr_in a;
-
+                
                 memcpy(&a, (c->ifa_addr), sizeof(struct sockaddr_in));
                 char *as = inet_ntoa(a.sin_addr);
-
+                //printf("%s\n", as);
+                
                 int apart;
                 sscanf(as, "%d", &apart);
                 if(apart > 0 && apart != 127) {
@@ -107,15 +112,15 @@ void *serviceConnection(void *arg) {
                 }
             }
         }
-
+        
         freeifaddrs(ifa);
         return 0;
     }
-
+    
     int main(int argc, char *argv[]) {
         int s = rcsSocket();
         struct sockaddr_in a;
-
+        
         memset(&a, 0, sizeof(struct sockaddr_in));
         a.sin_family = AF_INET;
         a.sin_port = 0;
@@ -123,18 +128,23 @@ void *serviceConnection(void *arg) {
             fprintf(stderr, "Could not get public ip address. Exiting...\n");
             exit(0);
         }
-
+        
         if(rcsBind(s, &a) < 0) {
             fprintf(stderr, "rcsBind() failed. Exiting...\n");
             exit(0);
         }
 
+	if(rcsGetSockName(s, &a) < 0) {
+            fprintf(stderr, "rcsGetSockName() failed. Exiting...\n");
+            exit(0);
+	}
+        
         printf("%s %u\n", inet_ntoa(a.sin_addr), ntohs(a.sin_port));
-
+        
         if(rcsListen(s) < 0) {
             perror("listen"); exit(0);
         }
-
+        
         memset(&a, 0, sizeof(struct sockaddr_in));
         int asock;
         while((asock = rcsAccept(s, (struct sockaddr_in *)&a)) > 0) {
@@ -142,13 +152,12 @@ void *serviceConnection(void *arg) {
             *newasock = asock;
             int err;
             pthread_t t;
-
+            
             if(err = pthread_create(&t, NULL, &serviceConnection, (void *)(newasock))) {
                 fprintf(stderr, "pthread_create(): %s\n", strerror(err));
                 exit(1);
             }
         }
-
+        
         return 0;
     }
-
